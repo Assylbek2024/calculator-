ad3626cc1f70594f406f7c4e8f02607a055bbb3b
Assylbek <maratov001@mail.ru>
2024-03-05 20:44:48 +0500

    first commit

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textfield2: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touched(_ sender: Any) {
+       // label.text = textfield.text
+        let a = textfield.text!
+        let b = textfield2.text!
+        let sum = Int(a)! + Int(b)!
+        label.text = "\(sum)"
+    }
+    
+    
+    @IBAction func touched2(_ sender: Any) {
+        let a = textfield.text!
+        let b = textfield2.text!
+        let min = Int(a)! - Int(b)!
+        label.text = "\(min)"
+    }
+    
+    @IBAction func touched3(_ sender: Any) {
+        let a = textfield.text!
+        let b = textfield2.text!
+        let del = Int(a)! / Int(b)!
+        label.text = "\(del)"
+    }
+    
+    @IBAction func touched4(_ sender: Any) {
+        let a = textfield.text!
+        let b = textfield2.text!
+        let umn = Int(a)! * Int(b)!
+        label.text = "\(umn)"
+    }
 }
 
